cec43223cfdba297def7b46997110245d813bc40
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-02-02 13:48:07 +0100

    statistics vizz

---
+++ app/scripts/app.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+
+    angular.module('app', []);
+
+    angular.module('app').controller('StatusController', StatusController);
+    angular.module('app').controller('PerformanceController', PerformanceController);
+
+    function StatusController($http) {
+        var vm = this;
+
+        vm.status = null;
+
+        $http({
+            method: 'GET',
+            url: 'http://production-api.globalforestwatch.org/api/v1/microservice/status'
+            }).then(function successCallback(response) {
+                vm.status = response.data;
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+        });
+
+    }
+
+    function PerformanceController($http) {
+        var vm = this;
+
+        vm.statistics = null;
+
+        $http({
+            method: 'GET',
+            url: 'http://staging-api.globalforestwatch.org/api/v1/stadistic/avgByRequest'
+            }).then(function successCallback(response) {
+                vm.statistics = response.data;
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+        });
+
+    }
+
+
+})();
